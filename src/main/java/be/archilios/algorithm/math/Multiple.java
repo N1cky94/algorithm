32cabdf4057c4d345e3947c7e94154c843f3ce12
Nick Bauters <nick.bauters@multimedi.be>
2023-07-16 19:52:49 +0200

    feat: Describe the first problem
    Described the first problem and created an empty main method to solve it spaghetti style.
    After the problem has been solved, we will be refactoring.

---
+++ src/main/java/be/archilios/algorithm/math/Multiple.java
@@ -0,0 +1,17 @@
+package be.archilios.algorithm.math;
+
+public class Multiple {
+    
+    /**
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+     * The sum of these multiples is 23.
+     *
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     *
+     * SPAGHETTI APROACH
+     *
+     */
+    public static void main(String[] args) {
+    
+    }
+}
