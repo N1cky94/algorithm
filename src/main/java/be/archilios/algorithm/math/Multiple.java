2c0658dbf08ebf230b8bc56aa5e97cccd3486a3e
Nick Bauters <nick.bauters@multimedi.be>
2023-07-16 20:03:39 +0200

    feat: Take sum of multiples of 3 and 5
    wrote a simple main method that calculates the sum of the multiples of 3 and 5.
    It works for all multiples smaller than 10 and 1000.

--- src/main/java/be/archilios/algorithm/math/Multiple.java
+++ src/main/java/be/archilios/algorithm/math/Multiple.java
@@ -12,6 +12,14 @@ public class Multiple {
      *
      */
     public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 10; i++) {
+            if ((i % 3 == 0) || (i % 5 == 0)) {
+                sum += i;
+                System.out.println("multiple = " + i);
+            }
+        }
     
+        System.out.println("sum = " + sum);
     }
 }
